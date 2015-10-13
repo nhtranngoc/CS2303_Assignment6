941fb749f54a03e2cf6d6208a08f8ae0832342f5
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2015-10-13 00:07:13 -0400

    Bonuses

--- cptest_bonus.c
+++ cptest_bonus.c
@@ -9,12 +9,14 @@
 #include <string.h>
 #include <sys/time.h>
 #include <libgen.h>
+#include <ctype.h>
 
-
+#define MAX_FILE_LENGTH 128
 // function prototype
 int copyfile1(char* infilename, char* outfilename);
 struct timeval * gettimeelapsed(struct timeval * start, struct timeval * end);
 void printtime(struct timeval * tm);
+void generateOutName(char* infilename, char* outfilename);
 
 /** cptest.cpp
  * A file copying program.
@@ -25,8 +27,7 @@ void printtime(struct timeval * tm);
    Prints usage instructions.
    @param program_name the name of this program
 */
-void usage(char* program_name)
-{  
+void usage(char* program_name){
   printf("Usage: %s infile outfile functionmode buffersize\n", program_name);
   printf("functionmode must be between 1 and 3.\n");
   printf("buffersize is only applicable with function mode 3.\n");
@@ -36,8 +37,7 @@ void usage(char* program_name)
    Prints file opening error message
    @param filename the name of the file that could not be opened
 */
-void open_file_error(char* filename)
-{  
+void open_file_error(char* filename){  
   printf("Error opening file %s: %d: %s\n", filename, errno, strerror(errno));
 }
 
@@ -46,81 +46,82 @@ void open_file_error(char* filename)
     @param argv Array of pointers to character arrays holding arguments.
     @return 0 if successful, 1 if fail.
 */
-int main(int argc, char* argv[])
-{  
+int main(int argc, char** argv){  
+  int functionmode = 1;
+  int bufSize = 1;
+  int returnVal=-1;
   char* infilename; // Names of files.
   char* outfilename;
-  int functionmode = 1;
-  int bufSize = 2048;
-  int returnVal=-1;
-  struct stat sb;
   struct timeval * tmStart = (struct timeval*)malloc(sizeof(struct timeval));
   struct timeval * tmEnd = (struct timeval*)malloc(sizeof(struct timeval));
   struct timeval * tmDiff = (struct timeval*)malloc(sizeof(struct timeval));
 
-  if (argc < 4) {
+  if (argc < 2){
     usage(argv[0]); // Must have exactly 2 arguments.
     return 1;
   }
 
-  infilename = argv[1];
-  outfilename = argv[2];
-  functionmode = atoi(argv[3]);
-  // bufSize = atoi(argv[4]);
+  int opt;
+  int index=0;
+  char *cvalue = (char *)malloc(sizeof(char));
+  while((opt = getopt(argc, argv, "b:c:")) != -1)
+    switch(opt){
+      case 'c': 
+        functionmode = atoi(optarg); 
+        printf("Mode C\n");
+      break;
 
-  if((functionmode>3)|(functionmode<1)){
-    usage(argv[0]);
-    return 1;
-  }
+      case 'b': 
+        printf("Mode B\n");
+        bufSize = atoi(optarg); 
+      break;
+      
+      case '?':
+        if (optopt == 'c')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
+        return 1;
 
- if (functionmode == 3 && argc == 5){
-    bufSize = atoi(argv[4]);
-  }
-
-  char* outdir;
-  char* filename;
-  if (stat(argv[2], &sb) == 0 && S_ISDIR(sb.st_mode)){
-    // printf("It's a directory!\n");
-    outdir = argv[2];
-    filename = basename(infilename);
-    outfilename = (char*)malloc(sizeof(char) * (strlen(outdir) + strlen(filename) + 2));
-    strcat(outfilename, outdir);
-    strcat(outfilename, "/");
-    strcat(outfilename, filename);
-    strcat(outfilename, "\0");
-  } else {
-    printf("It's a-me, Mario!\n");
-    outfilename = argv[2];
-  }
-
-  gettimeofday(tmStart, NULL);
-
-  // Perform the copying
-  switch (functionmode){
-    case 1:
-        returnVal = copyfile1(infilename, outfilename);
-        break;
-    case 2:
-        returnVal = copyfile2(infilename, outfilename);
-        break;
-    case 3:
-        returnVal = copyfile3(infilename, outfilename, bufSize);
-        break;
+      default:
+        abort();
     }
-    gettimeofday(tmEnd, NULL);
-    tmDiff = gettimeelapsed(tmStart, tmEnd);
-
-    printf("Time started: ");
-    printtime(tmStart);
-
-    printf("Time ended: ");
-    printtime(tmEnd);
-
-    printf("Time elapsed: ");
-    printtime(tmDiff);
-    free(tmStart);
-    free(tmEnd);
-    free(tmDiff);
+  char** argList = (char**)malloc(10 * sizeof(char*));
+  int count=0;
+  for (index = optind; index < argc; index++){
+    argList[count] = (char*)malloc(MAX_FILE_LENGTH * sizeof(char));
+    argList[count] = argv[index];
+    // printf("Argument: %s\n", argList[count]);
+    count++;
+  }
+  printf("Count: %d\n", count);
+  outfilename=argList[count-1];
+  printf("outfilename: %s\n", outfilename);
+  // generateOutName(infilename, outfilename);
+  gettimeofday(tmStart, NULL);
+  // Perform the copying
+  // returnVal = copy();
+  int i;
+  for(i=0;i<count-1;i++){
+    infilename=argList[i];
+    printf("infilename: %s\n", infilename);
+    generateOutName(infilename, outfilename);
+    printf("Generated outfilename: %s\n", outfilename);
+    returnVal=copy(infilename, outfilename, functionmode, bufSize);
+  }
+  gettimeofday(tmEnd, NULL);
+  tmDiff = gettimeelapsed(tmStart, tmEnd);
+  printf("Time started: ");
+  printtime(tmStart);
+  printf("Time ended: ");
+  printtime(tmEnd);
+  printf("Time elapsed: ");
+  printtime(tmDiff);
+  free(tmStart);
+  free(tmEnd);
+  free(tmDiff);
   return returnVal;
 }
 
@@ -129,26 +130,60 @@ void printtime(struct timeval * tm){
 }
 
 struct timeval * gettimeelapsed(struct timeval * start, struct timeval * end){
-    struct timeval * diff = (struct timeval *)malloc(sizeof(struct timeval));
-    diff->tv_sec = end->tv_sec - start->tv_sec;
-    diff->tv_usec = end->tv_usec - start->tv_usec;
-    
-    //Handle borrowing.
-    if (diff->tv_usec < 0){
-        diff->tv_sec -= 1;
-        diff->tv_usec += 1000000;
-    }
-
-    return diff;
+  struct timeval * diff = (struct timeval *)malloc(sizeof(struct timeval));
+  diff->tv_sec = end->tv_sec - start->tv_sec;
+  diff->tv_usec = end->tv_usec - start->tv_usec;
+  //Handle borrowing.
+  if (diff->tv_usec < 0){
+    diff->tv_sec -= 1;
+    diff->tv_usec += 1000000;
+  }
+  return diff;
 }
 
+int copy(char* infilename, char* outfilename, int functionmode, int bufSize){
+  int returnVal;
+  switch(functionmode){
+    case 1:
+      returnVal = copyfile1(infilename, outfilename);
+    break;
+
+    case 2:
+      returnVal = copyfile2(infilename, outfilename);
+    break;
+
+    case 3:
+      returnVal = copyfile3(infilename, outfilename, bufSize);
+    break;
+  }
+}
+
+/**
+ * Checks if infilename is a directory, generate outfilename accordingly
+ */
+void generateOutName(char* infilename, char* outfilename){
+  struct stat sb;
+  char* outdir;
+  char* filename;
+  if (stat(outfilename, &sb) == 0 && S_ISDIR(sb.st_mode)){
+  // printf("It's a directory!\n");
+    outdir = outfilename;
+    filename = basename(infilename);
+    outfilename = (char*)malloc(sizeof(char) * (strlen(outdir) + strlen(filename) + 2));
+    strcat(outfilename, outdir);
+    strcat(outfilename, "/");
+    strcat(outfilename, filename);
+    strcat(outfilename, "\0");
+    printf("Generated outfilename in func: %s\n", outfilename);
+  }
+}
 
 /** Copies one file to another using formatted I/O, one character at a time.
  @param infilename Name of input file
  @param outfilename Name of output file
  @return 0 if successful, 1 if error.
 */
-int copyfile1(char* infilename, char* outfilename) {
+int copyfile1(char* infilename, char* outfilename){
   FILE* infile; //File handles for source and destination.
   FILE* outfile;
 
@@ -189,7 +224,7 @@ int copyfile2(char* infilename, char* outfilename){
     return 1;
   }
   int outfile= open(outfilename,  O_CREAT | O_WRONLY,
-                                  S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
 
   if (outfile < 0){
     printf("Error %d in cptest.c: %s\n", errno, strerror(errno));
@@ -213,7 +248,7 @@ int copyfile3(char* infilename, char* outfilename, int bufSize){
     return 1;
   }
   int outfile= open(outfilename,  O_CREAT | O_WRONLY,
-                                  S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
 
   if (outfile < 0){
     printf("Error %d in cptest.c: %s\n", errno, strerror(errno));
--- makefile
+++ makefile
@@ -1,15 +1,22 @@
+all: cptest cptest_bonus
+
 cptest: cptest.o
 	gcc -g cptest.o -o cptest
 
 cptest.o: cptest.c
 	gcc -c -g cptest.c
 
+cptest_bonus: cptest_bonus.o
+	gcc -g cptest_bonus.o -o cptest_bonus
+
+cptest_bonus.o: cptest_bonus.c
+	gcc -c -g cptest_bonus.c
+
 clean:
 	rm -f *.o cptest
+	rm -f cptest_bonus
 
 docs:
 	doxygen
 	chmod a+r html/*
 	cp -p html/* ~/public_html/cs2303assig6
-
-
